9ee2c7380b1689a70e2d151ba413892adb51fae3
OstafiichukO <85893003+OstafiichukO@users.noreply.github.com>
2022-04-10 21:40:42 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,10 +1,33 @@
-const countText = document.querySelector("div");
-const startCountBtn = document.querySelector("button");
-let count = 10;
+const timeEl = document.querySelector("div");
+const btnEl = document.querySelector("button");
+btnEl.disabled = false;
 
-const counter = (count) => {
-  count--;
-  countText.innerHTML = count;
-};
+let timer;
+let time = 10;
 
-const timer = setInterval(counter, 1000);
+function count() {
+  timer = setTimeout(countdown, 1000);
+  btnEl.disabled = true;
+}
+
+function reset() {
+  clearTimeout(timer);
+  time = 10;
+  timeEl.innerHTML = "Count here down from 10 to 0";
+  btnEl.disabled = false;
+}
+
+function countdown() {
+  timeEl.innerHTML = time.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  time--;
+  if (time < 0) {
+    reset();
+  } else {
+    count();
+  }
+}
+
+btnEl.addEventListener("click", countdown);
